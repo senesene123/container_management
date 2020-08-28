78d28fd569b96d2dc3eff7b10746ed85f9ec7d4a
senesene123 <senecom123@yahoo.co.jp>
2020-08-29 07:44:39 +0900

    SENE-4 add flg validation

--- app/models/search_form.rb
+++ app/models/search_form.rb
@@ -4,11 +4,8 @@ class SearchForm
   include ActiveModel::Model
 
   attr_accessor :nippo_flg, :geppo_flg
-  validate :flg_check
 
   def flg_check
-    return true unless nippo_flg == '0' && geppo_flg == '0'
-
-    errors.add(:nippo_flg, '日報または月報にチェックを入れてください。')
+    errors.add(:base, '日報または月報にチェックを入れてください。') if nippo_flg == '0' && geppo_flg == '0'
   end
 end
